2ecf23a5bc78e4428e06400127a66c57b43b9c30
Nastya-Abakanovich <nastya.abakanovich@gmail.com>
2022-10-23 19:50:09 +0300

    Added example of use.

--- ExampleOfUse/Program.cs
+++ ExampleOfUse/Program.cs
@@ -8,25 +8,26 @@ namespace ExampleOfUse
         {
             Person person;
             var faker = new Faker();
-            for (int j = 0; j < 10; j++)
+            for (int j = 0; j < 5; j++)
             {
-                //Console.WriteLine("sbyte: {0}", faker.Create<sbyte>());
-                //Console.WriteLine("byte: {0}", faker.Create<byte>());
-                //Console.WriteLine("int: {0}", faker.Create<int>());
-                //Console.WriteLine("uint: {0}", faker.Create<uint>());
-                //Console.WriteLine("long: {0}", faker.Create<long>());
-                //Console.WriteLine("ulong: {0}", faker.Create<ulong>());
-                //Console.WriteLine("short: {0}", faker.Create<short>());
-                //Console.WriteLine("ushort: {0}", faker.Create<ushort>());
-                //Console.WriteLine("double: {0}", faker.Create<double>());
-                //Console.WriteLine("float: {0}", faker.Create<float>());
-                //Console.WriteLine("DateTime: {0}", faker.Create<DateTime>().ToString());
-                //Console.WriteLine("string: {0}", faker.Create<string>());
-                //Console.WriteLine("char: {0}", faker.Create<char>());
-                //Console.WriteLine("bool: {0}", faker.Create<bool>());
-                //Console.WriteLine();
+                Console.WriteLine("sbyte: {0}", faker.Create<sbyte>());
+                Console.WriteLine("byte: {0}", faker.Create<byte>());
+                Console.WriteLine("int: {0}", faker.Create<int>());
+                Console.WriteLine("uint: {0}", faker.Create<uint>());
+                Console.WriteLine("long: {0}", faker.Create<long>());
+                Console.WriteLine("ulong: {0}", faker.Create<ulong>());
+                Console.WriteLine("short: {0}", faker.Create<short>());
+                Console.WriteLine("ushort: {0}", faker.Create<ushort>());
+                Console.WriteLine("double: {0}", faker.Create<double>());
+                Console.WriteLine("float: {0}", faker.Create<float>());
+                Console.WriteLine("DateTime: {0}", faker.Create<DateTime>().ToString());
+                Console.WriteLine("string: {0}", faker.Create<string>());
+                Console.WriteLine("char: {0}", faker.Create<char>());
+                Console.WriteLine("bool: {0}", faker.Create<bool>());                
                 person = faker.Create<Person>();
-                Console.WriteLine("name: {0}, surname: {1}, Birhday: {2}, id: {3}  AGE: {4}", person.Name, person.Sername, person.BirthDate, person.Id, person.Age);
+                Console.WriteLine("id: {0}, name: {1}, surname: {2}, birhday: {3}, isFemale: {4}, child: {5}", person.Id, person.Name, 
+                                  person.Sername, person.BirthDate, person.IsFemale, person.Child != null ? "not null" : "null");
+                Console.WriteLine();
             }
         }
                 
@@ -34,24 +35,29 @@ namespace ExampleOfUse
 
     public class Person
     {
-        public DateTime BirthDate { get; set; }
+        public ushort Id { get; }
+        
         public string Name { get; set; }
-        public ushort Id { get;}
         public string Sername { get; set; }
-        public byte Age;
+        public DateTime BirthDate { get; set; }
+
+        public Person Child;
+        public bool IsFemale;
 
         public Person ()
         {
             BirthDate = DateTime.Now;
             Name = "N";
-            Sername = "F";
+            Sername = "S";
             Id = 8;
+            Child = null;
+            IsFemale = true;
         }
 
         public Person(ushort id)
         {
             Id = id;
-            Name = "W";
+            Name = "Const name.";
         }
     }
 }
