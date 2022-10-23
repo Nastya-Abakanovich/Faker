532b1da58e781d8be1fd227e8cec782b54bdf8e5
Nastya-Abakanovich <nastya.abakanovich@gmail.com>
2022-10-23 20:29:02 +0300

    Class Person in a separate file.

--- ExampleOfUse/Program.cs
+++ ExampleOfUse/Program.cs
@@ -32,32 +32,4 @@ namespace ExampleOfUse
         }
                 
     }
-
-    public class Person
-    {
-        public ushort Id { get; }
-        
-        public string Name { get; set; }
-        public string Sername { get; set; }
-        public DateTime BirthDate { get; set; }
-
-        public Person Child;
-        public bool IsFemale;
-
-        public Person ()
-        {
-            BirthDate = DateTime.Now;
-            Name = "N";
-            Sername = "S";
-            Id = 8;
-            Child = null;
-            IsFemale = true;
-        }
-
-        public Person(ushort id)
-        {
-            Id = id;
-            Name = "Const name.";
-        }
-    }
 }
